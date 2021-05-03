7dec6e12095a273249a10351cd3c9e64146acfb1
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-05-03 16:43:42 -0300

    created model devise e db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reservas do
-    resources :usuarios
+  resources :reservas
   end
 end
