651eacf37e21061be55063b892144f349d3d4a3f
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-05-04 00:02:31 -0300

    added simple form with bootstrap and updated controllers, customized views login authentication, added scss styles to alerts

--- app/controllers/recomendations_controller.rb
+++
@@ -1,2 +0,0 @@
-class RecomendationsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :reservations
+  # resources :reservations
   root to: 'reservations#index'
 end
